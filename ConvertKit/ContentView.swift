b2231a5db08eb7ba9626d10cf53836d0af768860
Miten Gajjar <miten@lenditt.com>
2023-08-20 16:21:49 +0530

    Initial Commit

---
+++ ConvertKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConvertKit
+//
+//  Created by Miten Gajjar on 20/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
